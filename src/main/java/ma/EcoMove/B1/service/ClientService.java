553319091ddfd2d511db7d0eca0a8755c09a5cb0
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-13 10:27:14 +0100

    working client service

--- src/main/java/ma/EcoMove/B1/service/ClientService.java
+++ src/main/java/ma/EcoMove/B1/service/ClientService.java
@@ -3,8 +3,9 @@ package main.java.ma.EcoMove.B1.service;
 import main.java.ma.EcoMove.B1.dao.Interface.IClient;
 import main.java.ma.EcoMove.B1.entity.Client;
 import main.java.ma.EcoMove.B1.service.IService.IClientService;
-
+import java.time.LocalDate;
 import java.util.Optional;
+import java.util.UUID;
 
 public class ClientService  implements IClientService {
     private final IClient clientDAO ;
@@ -17,8 +18,23 @@ public class ClientService  implements IClientService {
     public Optional<Client> findByEmail(String email){
         return clientDAO.findByEmail(email);
     }
-@override
-    public Client registerClient (String nom , String prenom, String email, String telephone){
+
+    public Client registerClient (String nom , String prenom, String email, String telephone , LocalDate dateInscription ){
+
+        Client client = new Client (
+                UUID.randomUUID(),
+                nom,
+                prenom,
+                email,
+                telephone,
+                LocalDate.now(),
+                null
+
+
+
+        );
+        clientDAO.save(client);
+                return client;
 
     }
 
